3c54aac08d43ea1cd828b78ae9ca5e5800a782d3
silentroar23 <theforgotten23623@gmail.com>
2020-12-10 22:46:34 +0800

    Complete three dfs problems, partial sum, cycle detection and bipartite check

--- AcWing/fundermental_algorithms/searching_and_graph/shortest_path/dijkstra/naive_dijkstra.cpp
+++ AcWing/fundermental_algorithms/searching_and_graph/shortest_path/dijkstra/naive_dijkstra.cpp
@@ -3,30 +3,38 @@
 
 using namespace std;
 
+// the graph is composed of n nodes and m edges, and m ~ n ^ 2 
 const int N = 510;
-int g[N][N];
-int dist[N];
-bool st[N];
+int dist[N];  // shortest distance between node 1 to node i
+int g[N][N];  // use adjacent matrix to store the graph
+bool st[N];  // state of each node
 int n, m;
 
 int dijkstra()
 {
+    // 1. set distance between node 1 and node i to inf, and set distance between node 1 and 1 to 0
     memset(dist, 0x3f, sizeof dist);
     dist[1] = 0;
     
+    // 2. outer loop
+    // loop n times, after each loop, a node's shortest distance to node 1 is calculated
     for ( int i = 0; i < n; i ++ )
     {
-        int t = -1;
+        // 3. calculate the node with the shortest diatance to node 1
+        int t = -1;  // the node that has the shortest distance to node 1 might be node 1
         for ( int j = 1; j <= n; j ++ )
         {
-            if ( !st[j] && ( t == -1 || dist[j] < dist[t] ))
+            if ( !st[j] and ( t == -1 or dist[j] < dist[t] ) )
                 t = j;
         }
-        
+
+        // 4. mark the node as completed
         st[t] = true;
-        
+
         for ( int j = 1; j <= n; j ++ )
         {
+            // 5. update the distance of each node
+            // note g[t][j] cannot be mistaken for g[j][t]
             dist[j] = min(dist[j], dist[t] + g[t][j]);
         }
     }
@@ -37,15 +45,17 @@ int dijkstra()
 
 int main()
 {
+    // set the distance between each node to inf
     memset(g, 0x3f, sizeof g);
     cin >> n >> m;
     while ( m -- )
     {
         int x, y, z;
         cin >> x >> y >> z;
+        // only need to store the shortest edge
         g[x][y] = min(g[x][y], z);
     }
-    
+
     cout << dijkstra() << endl;
     return 0;
 }
--- LeetCode/2_add_numbers.cpp
+++ LeetCode/2_add_numbers.cpp
@@ -1,3 +1,11 @@
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
 class Solution
 {
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
